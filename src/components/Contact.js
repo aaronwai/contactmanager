d01e379d55563f60f03a1f468e6f7ce917291076
aaron Wai <aaron.lung.wai@gmail.com>
2023-04-23 13:54:17 +0800

    Step 2 : class component

---
+++ src/components/Contact.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h4>John</h4>
+        <ul>
+          <li>email : a@a.com</li>
+          <li>phone : 12345678</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Contact;
