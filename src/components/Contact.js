4f0f36ccd95b6aaf705fede730f1d565ae624789
aaron Wai <aaron.lung.wai@gmail.com>
2023-04-24 10:18:00 +0800

    Step 11 : event changing the state application

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,28 +6,27 @@ class Contact extends Component {
   static propTypes = {
     contact: PropTypes.object.isRequired,
   };
-  state = {};
-  // add id parameter to the event callback function, the parameter as first argument and event as the last argument link to the event callback.
-  // or name parameter to the event callback function, change the 1 to name @ line 24
-  onShowClick = (id, e) => {
-    console.log(e.target);
-    console.log(id);
+
+  state = {
+    showContactInfo: true,
+  };
+  onShowClick = (e) => {
+    this.setState({ showContactInfo: !this.state.showContactInfo });
   };
   render() {
     const { name, email, phone } = this.props.contact;
+    const { showContactInfo } = this.state;
     return (
       <div className='card card-body mb-3'>
         <h4>
-          {name}{" "}
-          <i
-            className='fas fa-sort-down'
-            onClick={this.onShowClick.bind(this, name)}
-          ></i>
+          {name} <i className='fas fa-sort-down' onClick={this.onShowClick}></i>
         </h4>
-        <ul className='list-group'>
-          <li className='list-group-item'>Email : {email}</li>
-          <li className='list-group-item'>Phone : {phone}</li>
-        </ul>
+        {showContactInfo ? (
+          <ul className='list-group'>
+            <li className='list-group-item'>Email : {email}</li>
+            <li className='list-group-item'>Phone : {phone}</li>
+          </ul>
+        ) : null}
       </div>
     );
   }
