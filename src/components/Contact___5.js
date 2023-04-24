70ca9b88c953aa7bde28f53277caec71ffbe0ce5
aaron Wai <aaron.lung.wai@gmail.com>
2023-04-24 09:43:01 +0800

    Step 10 : event to change state with id parameter

--- src/components/Contact___5.js
+++ src/components/Contact___5.js
@@ -7,22 +7,17 @@ class Contact extends Component {
     contact: PropTypes.object.isRequired,
   };
   state = {};
-  // add id parameter to the event callback function, the parameter as first argument and event as the last argument link to the event callback.
-  // or name parameter to the event callback function, change the 1 to name @ line 24
-  onShowClick = (id, e) => {
+  // how event related to the event method, event is the the object that trigger and bind. for this case, event is from the icon element tab
+  //  console.log will print the event element tab
+  onShowClick = (e) => {
     console.log(e.target);
-    console.log(id);
   };
   render() {
     const { name, email, phone } = this.props.contact;
     return (
       <div className='card card-body mb-3'>
         <h4>
-          {name}{" "}
-          <i
-            className='fas fa-sort-down'
-            onClick={this.onShowClick.bind(this, name)}
-          ></i>
+          {name} <i className='fas fa-sort-down' onClick={this.onShowClick}></i>
         </h4>
         <ul className='list-group'>
           <li className='list-group-item'>Email : {email}</li>
